51c48d205bff7623e5b5a5d39981081b567c2679
Alexey <screenager16@gmail.com>
2023-11-09 11:48:02 +0700

    Erase

--- client/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/awl/projects/webchat/server
-# Build directory: /home/awl/projects/webchat/server/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- client/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "chat.h"
- 
-int main()
-{
-    // system("echo `date +'%H:%M:%S %d/%m/%Y'` >> data/msgData");
-    LocalDB db;
-    Chat chat(&db);
-    chat.run();
-}
